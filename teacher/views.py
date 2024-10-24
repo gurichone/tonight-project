05b582aeec4b807a2bd847d5707f712ddcf0e17c
kotat5u <166573071+kotat5u@users.noreply.github.com>
2024-10-24 05:31:40 +0000

    はじめようか

--- teacher/views.py
+++ teacher/views.py
@@ -1,7 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, current_app
-from gemini.forms import GeminiForms, UploadImageForm, JupyterForms
-from pathlib import Path
-from gemini.models import Codes
+from flask import Blueprint, render_template, redirect, url_for
 from app import db
 
 #Blueprint currydar_app
@@ -13,9 +10,30 @@ teacher = Blueprint(
     static_folder="static",
 )
 
-from test import views as test_views
-teacher.register_blueprint(test_views.test, url_prefix="/test")
 
-@teacher.route("/")
-def teacherDAO():
-    return "teacherDAO"
+from teacher_menu import views as teacher_menu_views
+teacher.register_blueprint(teacher_menu_views.menu, url_prefix="/")
+
+from teacher_student import views as teacher_student_views
+teacher.register_blueprint(teacher_student_views.student, url_prefix="/student")
+
+from teacher_seiseki import views as teacher_seiseki_views
+teacher.register_blueprint(teacher_seiseki_views.seiseki, url_prefix="/seiseki")
+
+from teacher_teisyutu import views as teacher_teisyutu_views
+teacher.register_blueprint(teacher_teisyutu_views.teisyutu, url_prefix="/teisyutu")
+
+from teacher_jikanwari import views as teacher_jikanwari_views
+teacher.register_blueprint(teacher_jikanwari_views.jikanwari, url_prefix="/jikanwari")
+
+from teacher_saiten import views as teacher_saiten_views
+teacher.register_blueprint(teacher_saiten_views.saiten, url_prefix="/saiten")
+
+from teacher_chat import views as teacher_chat_views
+teacher.register_blueprint(teacher_chat_views.chat, url_prefix="/chat")
+
+from teacher_notice import views as teacher_notice_views
+teacher.register_blueprint(teacher_notice_views.notice, url_prefix="/notice")
+
+from teacher_sent import views as teacher_sent_views
+teacher.register_blueprint(teacher_sent_views.sent, url_prefix="/sent")
---
+++ teacher_menu/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, redirect, url_for
+from app import db
+
+menu = Blueprint(
+    "menu",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+@menu.route("/")
+def t_menu():
+    return "tounai is GOD"
