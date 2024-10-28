54d4fda85b5e9ce5c587bb18c3008017a0f72385
kotat5u <166573071+kotat5u@users.noreply.github.com>
2024-10-28 05:22:41 +0000

    背景がつくよ

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,15 @@
-from app import db 
-from auth.models import Teacher, Student 
 from flask import Blueprint, flash, redirect, render_template, request, url_for
+from app import db 
+from admin.forms import CourseForm, SubjectForm
+from auth.models import Teacher, Student 
+from teacher_teisyutu.models import Subject, Course, Submission, SubmissionSituation
+
 
 admin = Blueprint("admin", __name__, template_folder="templates", static_folder="static")
+
+@admin.route("/")
+def admin_is_god():
+    course_form = CourseForm()
+    if course_form.validate_on_submit():
+        return "courseテーブルに保存しました"
+    return render_template("admin/admin.html", course_form=course_form)
--- teacher_teisyutu/models.py
+++ teacher_teisyutu/models.py
@@ -1,24 +1,23 @@
 from app import db
-from auth.models import Student
 
 class Subject(db.Model):
     __tablename__ = "subject"
     id = db.Column(db.String, primary_key=True)
-    subject_id = db.Column(db.String, nullable=False)
-    subject_name = db.Column(db.String, nullable=False)
-    course_name = db.Column(db.String)
+    subject_id = db.Column(db.String(10), nullable=False)
+    subject_name = db.Column(db.String(20), nullable=False)
+    course_name = db.Column(db.String(30))
 
 class Course(db.Model):
     __tablename__ = "course"
-    class_num = db.Column(db.String, primary_key=True)
-    course_name = db.Column(db.String, nullable=False)
+    class_num = db.Column(db.String(4), primary_key=True)
+    course_name = db.Column(db.String(30), nullable=False)
 
 class Submission(db.Model):
     __tablename__ = "submission"
     submission_id = db.Column(db.String, primary_key=True)
     submission_name = db.Column(db.String, nullable=False)
-    subject_id = db.Column(db.String, nullable=False)
-    course_name = db.Column(db.String, nullable=False)
+    subject_id = db.Column(db.String, db.ForeignKey('subject.subject_id'), nullable=False)
+    course_name = db.Column(db.String, db.ForeignKey('course.course_name'), nullable=False)
     submission_type = db.Column(db.String, nullable=False)
     submissin_rimit = db.Column(db.DateTime, nullable=False)
     scoreing_type = db.Column(db.Integer, nullable=False)
@@ -27,8 +26,8 @@ class Submission(db.Model):
 
 class SubmissionSituation(db.Model):
     __tablename__ = "submission_situation"
-    submission_id = db.Column(db.String, primary_key=True)
-    student_id = db.Column(db.String, nullable=False)
+    submission_id = db.Column(db.String, db.ForeignKey('submission.submission_id'), primary_key=True)
+    student_id = db.Column(db.String, db.ForeignKey('student.id'), nullable=False)
     point = db.Column(db.Integer)
     submitted = db.Column(db.Boolean, nullable=False, default=0)
     file = db.Column(db.String)
