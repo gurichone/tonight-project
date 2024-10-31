d765e2ca6296cf7319ad5c836ac15afaf41c68a9
fujimori1125 <oky2384849@stu.o-hara.ac.jp>
2024-10-31 05:43:04 +0000

    検索結果を表示できるようになりました

--- teacher_seiseki/models.py
+++ teacher_seiseki/models.py
@@ -1,13 +1,14 @@
 from app import db
 
 # このデータベースは変更する必要がある(2024/10/30現在)
+# #をつけたカラムは他のデータベースを使用してhtmlに表示できるようにしたい
 class Score(db.Model):
     __tablename__ = "score"
     score_id = db.Column(db.Integer, primary_key=True)
     class_num = db.Column(db.String)
-    student_num = db.Column(db.String)#
-    student_name = db.Column(db.String)
+    student_num = db.Column(db.String)
+    student_name = db.Column(db.String)#
     subject_id = db.Column(db.String(10))
-    subject_name = db.Column(db.String)
-    assessment_id = db.Column(db.String(1))#
+    subject_name = db.Column(db.String)#
+    assessment_id = db.Column(db.String(1))
     attend_day = db.Column(db.Integer)
