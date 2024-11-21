a94c5ba0bb7154fe8c81cfa3b0599322c98115a6
fujimori1125 <oky2384849@stu.o-hara.ac.jp>
2024-11-21 01:44:40 +0000

    これでrunできるデマーーーシアーーーーーー

--- student_syusseki/views.py
+++ student_syusseki/views.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_required
 from datetime import datetime, timedelta
 from teacher_seiseki.models import Score
 from auth.models import Student
-import hashlib
+from student_syusseki.forms import AttendCheck
 
 syusseki = Blueprint(
     "syusseki",
@@ -13,32 +13,49 @@ syusseki = Blueprint(
     static_folder="static",
 )
 
+def is_code_valid():
+    code_timestamp = session.get('code_timestamp')
+    if code_timestamp:
+        # コードが発行されてから1分が経過したかを確認
+        issue_time = datetime.fromtimestamp(code_timestamp)
+        if datetime.now() <= issue_time + timedelta(minutes=1):
+            return True  # コードは有効
+    return False  # コードは無効
 
 @syusseki.route("/", methods=["GET", "POST"])
 @login_required
 def s_syusseki():
     if len(current_user.id) != 7:
         return render_template("student/gohb.html")
-    if request.method == "POST":
-        input_code = request.form.get("attendance_code")
-        
-        # 一時保存されたコードと時刻を取得
-        saved_code = session.get("attendance_code")
-        code_timestamp = session.get("code_timestamp")
+    
+    # フォームのインスタンスの作成
+    attend = AttendCheck()
+    
+    # ここでページに入力されたコードを取得
+    attend_code = request.form.get("attendance_code")
+    # 一時保存されたコードと時刻を取得
+    saved_code = session.get("attendance_code")
+    code_timestamp = session.get("code_timestamp")
 
+    if attend.validate_on_submit():
         # 1分の有効期限チェック
         if saved_code and code_timestamp:
-            time_diff = datetime.now().timestamp() - code_timestamp
-            if time_diff <= 60:  # 1分以内なら有効
-                if input_code == saved_code:
+            if is_code_valid():  # 1分以内なら有効
+                if attend_code == saved_code:
                     # 出席を記録する処理
-                    student_id = request.form.get("student_id")  # 生徒IDを入力させるか別途取得
+                    student_id = db.session.query(Student).filter(current_user.id == Student.id).first()  # 生徒IDを取得
                     score = db.session.query(Score).filter_by(id=student_id).first()
                     
                     if score:
                         score.attend_day += 1
                         db.session.commit()
-                        flash("出席が登録されました。", "success")
+                        return render_template(
+                            "student_syusseki/attend_complete.html",
+                            attend=attend,
+                            student_id=student_id,
+                            score=score
+                            )
+                    # 諸エラー
                     else:
                         flash("生徒IDが見つかりません。", "error")
                 else:
