bfd26e45a20e39d2c40d6048e037e1278b200622
kotat5u <166573071+kotat5u@users.noreply.github.com>
2024-11-22 03:14:30 +0000

    JavaScript最強! JavaScript最強! JavaScript最強!!!!

--- student_notice/views.py
+++ student_notice/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
 from app import db
+from teacher_sent.models import Information, Get_Information
+from auth.models import Teacher
 
 
 notice = Blueprint(
@@ -12,4 +15,5 @@ notice = Blueprint(
 
 @notice.route("/")
 def s_notice():
-    return "notice"
+    notice=db.session.query(Information, Get_Information, Teacher).join(Information, Teacher.id==Information.teacher_num).order_by("sent_time").join(Get_Information, Get_Information.info_id==Information.info_id).filter_by(student_num=current_user.id).all()
+    return render_template("student_notice/notice.html", notice=notice)
