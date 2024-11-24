3e6226f78a31b147e197be22fab6501223566a2d
kotat5u <166573071+kotat5u@users.noreply.github.com>
2024-11-24 17:07:35 +0000

    通知に既読機能追加

--- student_menu/views.py
+++ student_menu/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, current_app
 from flask_login import current_user, login_required
 from app import db
+from teacher_sent.models import Get_Information
 import os
 
 
@@ -16,11 +17,14 @@ menu = Blueprint(
 @menu.route("/")
 @login_required
 def s_menu():
+    if len(current_user.id) != 7:
+        return render_template("student/gohb.html")
     # ログインしている生徒の情報を取得
     student = current_user
-
+    # 未読の通知があるか確認
+    new = db.session.query(Get_Information).filter_by(student_num=student.id, read=0).all()
     # ユーザーがアイコンを設定していない場合はデフォルトのパスを使用
     if not student.icon_path or not os.path.exists(os.path.join(current_app.root_path, student.icon_path)):
         student.icon_path = 'uploads/icon/default/default_icon.png'
 
-    return render_template('student_menu/menu.html', student=student)
+    return render_template('student_menu/menu.html', student=student, new=len(new))
--- student_notice/views.py
+++ student_notice/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 from app import db
 from teacher_sent.models import Information, Get_Information
 from auth.models import Teacher
@@ -14,6 +14,15 @@ notice = Blueprint(
 
 
 @notice.route("/")
+@login_required
 def s_notice():
+    if len(current_user.id) != 7:
+        return render_template("student/gohb.html")
     notice=db.session.query(Information, Get_Information, Teacher).join(Information, Teacher.id==Information.teacher_num).order_by("sent_time").join(Get_Information, Get_Information.info_id==Information.info_id).filter_by(student_num=current_user.id).all()
+    # 未読のデータを取り出し既読にする
+    new = db.session.query(Get_Information).filter_by(student_num=current_user.id, read=0).all()
+    for n in new:
+        n.read=1
+        db.session.add(n)
+        db.session.commit()
     return render_template("student_notice/notice.html", notice=notice)
